5e31f1f31dcb469d4a72c2c11ebfa8a1ffd2b4aa
Grigoriy Kapranov <kapranovgu@mail.ru>
2023-01-20 22:35:31 +0300

    Task 5* from lesson

---
+++ Task5_from_lesson/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Напишите программу, которая генерирует последовательность 
+случайных чисел из 10 элементов в диапазоне от 1 до 10, 
+и подсчитывает, сколько сгенерировалось чисел больше 5.*/
+
+Random rnd = new Random();
+
+int count = 0;
+
+int[] listNumbers = new int[10];
+for (int i = 0; i < listNumbers.Length; i++)
+{
+    listNumbers[i] = rnd.Next(1, 10);
+    if (listNumbers[i] > 5)
+    {
+        count++;
+    }
+}
+
+System.Console.WriteLine(string.Join(",", listNumbers));
+System.Console.WriteLine($"Количество чисел больше 5: {count};");
+
